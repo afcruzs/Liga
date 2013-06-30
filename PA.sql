67bd5b08b5c18b5b05624fef1a3713aecf5bd374
Andrés Felipe Cruz <bones.felipe@gmail.com>
2013-06-30 14:55:08 -0500

    Actualización Modelo, PA y el Script
    Con los cambios que hablamos hoy cambie los PA y arregle algo del modelo
    y en el script porque habían atributos sin auto_increment que eran
    necesarios. En los PA puse en orden la inserción de los PA que hice.

---
+++ PA.sql
@@ -0,0 +1,240 @@
+/*
+##PA CON EL MODELO 'VIEJO'
+#Procedimiento para insertar jugadores (Los inserta también en el historico)
+DELIMITER $$
+
+CREATE PROCEDURE insertar_jugador (nacionalidad VARCHAR(45), edad INT, año INT, numero_c 
+									INT, nombres VARCHAR(45), apellidos VARCHAR(45), nombre_equ VARCHAR(45), numero_goles INT)
+	BEGIN	
+	START TRANSACTION;
+		SET @id_eq = (SELECT id_equipo FROM equipo WHERE nombre_equipo = nombre_equ);
+
+		INSERT INTO jugador (nacionalidad_jugador, edad_jugador,nombres_jugador, apellidos_jugador) 
+					VALUES (nacionalidad,edad,nombres,apellidos);
+
+		SET @id_jug = (SELECT id_jugador FROM jugador 
+					   WHERE nombres_jugador = nombres AND apellidos_jugador = apellidos );
+
+		INSERT INTO historico_jugadores VALUES (@id_jug,@id_eq,año,numero_c, numero_goles);
+	COMMIT;
+	END; 
+$$
+DELIMITER ;
+
+#Procedimiento para insertar equipos (Los inserta también en el historico)
+#DROP PROCEDURE insertar_equipo;
+DELIMITER $$
+CREATE PROCEDURE insertar_equipo (ciudad VARCHAR(45), nombre VARCHAR(45), año INT, rendimiento VARCHAR(45))
+	BEGIN
+	START TRANSACTION;
+		INSERT INTO equipo (ciudad_equipo, nombre_equipo) VALUES (ciudad,nombre,rendimiento);
+		SET @id = (SELECT id_equipo FROM equipo WHERE nombre_equipo = nombre);
+	COMMIT;
+	END;
+$$
+DELIMITER ;
+#Procedimiento para insertar entrenadores (Los inserta también en el histórico)
+DELIMITER $$
+CREATE PROCEDURE insertar_entrenador(telefono VARCHAR(45), equipo VARCHAR(45), experiencia INT, 
+									apellidos VARCHAR(45), nombres VARCHAR(45), año INT, salario DOUBLE)
+	BEGIN
+	START TRANSACTION;
+		INSERT INTO entrenador (telefono_entrenador, experiencia_entrenador, apellidos_entrenador, nombres_entrenador)
+						 VALUES( telefono, experiencia, apellidos, nombres );
+	
+	SET @id_ent = (SELECT id_entrenador FROM entrenador WHERE nombres_entrenador = nombres);
+	SET @id_equ = (SELECT id_equipo FROM equipo WHERE nombre_equipo = equipo);
+	INSERT INTO historico_entrenadores (salario_historico_entrenadores, año_historico_entrenadores, entrenador_id_entrenador, id_equipo) 
+				VALUES (salario, año, @id_ent, @id_equ);
+	COMMIT;
+	END;
+$$
+DELIMITER ;
+
+##NEW STUFFF
+
+DELIMITER $$
+CREATE PROCEDURE insertar_tecnico(telefono VARCHAR(45), experiencia INT, apellidos VARCHAR(45),
+									nombres VARCHAR(45), equipo VARCHAR(45) )
+	BEGIN
+	START TRANSACTION;
+	INSERT INTO tecnico (telefono_tecnico,experiencia_tecnico, apellidos_tecnico, nombres_tecnico)
+				  VALUES( telefono, experiencia, apellidos, nombres );
+
+	SET @id_tec = (SELECT COUNT(*) FROM tecnico);
+	SET @id_equ = (SELECT id_equipo FROM equipo WHERE nombre_equipo = equipo);
+
+	UPDATE equipo SET tecnico_id_tecnico = @id_tec WHERE  id_equipo = @id_equ;	
+	COMMIT;	
+	END;
+$$
+DELIMITER ; */
+									
+#####################################################################################################
+#PA CON EL MODELO 'NUEVO' PASANDO COLUMNAS A EQUIPO Y ENTRENADOR
+DROP PROCEDURE insertar_jugador;
+DELIMITER $$
+
+CREATE PROCEDURE insertar_jugador (nacionalidad VARCHAR(45), edad INT,  
+								apellidos VARCHAR(45), nombres VARCHAR(45), nombre_equ VARCHAR(45), numero_goles INT, numero_camiseta INT, fecha DATETIME )
+	BEGIN	
+	START TRANSACTION;
+		SET @id_equ = (SELECT id_equipo FROM equipo WHERE nombre_equipo = nombre_equ);
+		INSERT INTO jugador (nacionalidad_jugador, edad_jugador,nombres_jugador, apellidos_jugador, goles_jugador, numero_jugador, fechaNacimiento_jugador, id_equipo) 
+					VALUES (nacionalidad,edad,nombres,apellidos, numero_goles, numero_camiseta, fecha, @id_equ);
+	COMMIT;
+	END;
+$$
+DELIMITER ;
+
+DROP PROCEDURE insertar_equipo;
+DELIMITER $$
+CREATE PROCEDURE insertar_equipo (ciudad VARCHAR(45), nombre VARCHAR(45), rendimiento VARCHAR(45), apellido_tec VARCHAR(45), nombre_tec VARCHAR(45))
+	BEGIN
+	START TRANSACTION;
+		SET @id_tec = (SELECT id_tecnico FROM tecnico WHERE nombres_tecnico = nombre_tec  AND apellidos_tecnico = apellido_tec );
+		INSERT INTO equipo (ciudad_equipo, nombre_equipo, rendimiento_equipo, id_tecnico) VALUES (ciudad,nombre,rendimiento, @id_tec);
+		SET @id = (SELECT id_equipo FROM equipo WHERE nombre_equipo = nombre);
+	COMMIT;
+	END;
+$$
+DELIMITER ;
+
+
+
+DELIMITER $$
+CREATE PROCEDURE insertar_entrenador(telefono VARCHAR(45), equipo VARCHAR(45), experiencia INT, 
+									apellidos VARCHAR(45), nombres VARCHAR(45), año INT, salario DOUBLE)
+	BEGIN
+	START TRANSACTION;
+		INSERT INTO entrenador (telefono_entrenador, experiencia_entrenador, apellidos_entrenador, nombres_entrenador, salario_entrenador)
+						 VALUES( telefono, experiencia, apellidos, nombres, salario );
+	COMMIT;
+	END;
+$$
+DELIMITER ;
+
+DROP PROCEDURE insertar_tecnico;
+DELIMITER $$
+CREATE PROCEDURE insertar_tecnico(telefono VARCHAR(45), experiencia INT, apellidos VARCHAR(45),
+									nombres VARCHAR(45), salario DOUBLE)
+	BEGIN
+	START TRANSACTION;
+	INSERT INTO tecnico (telefono_tecnico,experiencia_tecnico, apellidos_tecnico, nombres_tecnico, salario_tecnico)
+				  VALUES( telefono, experiencia, apellidos, nombres, salario );
+	COMMIT;	
+	END;
+$$
+DELIMITER ;
+
+##########################################################################################
+##########################################################################################
+#Otros PA de inserción
+
+DROP PROCEDURE insertar_arbitro;
+DELIMITER $$
+CREATE PROCEDURE insertar_arbitro( nombre VARCHAR(45), apellido VARCHAR(45), tipo VARCHAR(45) )
+	BEGIN
+	START TRANSACTION;
+	INSERT INTO arbitro ( nombre_arbitro, apellido_arbitro, tipo_arbitro )VALUES( nombre, apellido, tipo );
+	COMMIT;
+	END;
+$$
+DELIMITER ;
+DROP PROCEDURE insertar_partido;
+DELIMITER $$
+CREATE PROCEDURE insertar_partido( fecha DATETIME , ciudad VARCHAR(45), estadio VARCHAR(45), goles_loc INT, 
+								   goles_vis INT, eq_local VARCHAR(45), eq_visitante VARCHAR(45), añ INT, sem INT)
+	BEGIN 
+	START TRANSACTION;
+		SET @camp = (SELECT id_campeonato FROM campeonato WHERE semestre = sem AND año = añ);
+		SET @id_loc = (SELECT id_equipo FROM equipo WHERE nombre_equipo = eq_local);
+		SET @id_vis = (SELECT id_equipo FROM equipo WHERE nombre_equipo = eq_visitante);
+
+		INSERT INTO partido (fecha_partido, ciudad_partido, estadio, goles_local, goles_visitante, id_local, id_visitante, id_campeonato  ) 
+					 VALUES (fecha, ciudad, estadio, goles_loc, goles_vis, @id_loc, @id_vis, @camp);
+	COMMIT;
+	END;
+$$
+DELIMITER ;
+DROP PROCEDURE insertar_campeonato;
+DELIMITER $$
+CREATE PROCEDURE insertar_campeonato(añ INT, sem INT)
+	BEGIN
+	START TRANSACTION;
+		INSERT INTO campeonato (año, semestre) VALUES (añ, sem);
+	COMMIT;
+	END;
+$$
+DELIMITER ;
+DROP PROCEDURE asingar_partido_arbitro;
+DELIMITER $$
+
+CREATE PROCEDURE asingar_partido_arbitro(nombre VARCHAR(45), apellido VARCHAR(45), nombre_loc VARCHAR(45), 
+										 nombre_vis VARCHAR(45), añ VARCHAR(45), sem VARCHAR(45))
+	BEGIN
+	START TRANSACTION;
+	SET @id_arbitro = (SELECT id_arbitro FROM arbitro WHERE nombre_arbitro = nombre AND apellido_arbitro = apellido);
+	SET @id_camp = (SELECT id_campeonato FROM campeonato WHERE semestre = sem AND año = añ);
+	SET @id_local = (SELECT id_equipo FROM equipo WHERE nombre_equipo = nombre_loc );
+	SET @id_vis = (SELECT id_equipo FROM equipo WHERE nombre_equipo = nombre_vis );
+	SET @id_partido = (SELECT id_partido FROM partido WHERE id_local = @id_local AND id_visitante = @id_vis 
+					   AND id_campeonato = @id_camp);
+	
+	INSERT INTO arbitro_partido VALUES(@id_arbitro, @id_partido);
+	
+	COMMIT;
+	END;
+$$
+DELIMITER ;
+
+DROP PROCEDURE insertar_equipo_en_campeonato;
+DELIMITER $$
+
+CREATE PROCEDURE insertar_equipo_en_campeonato(nombre_eq VARCHAR(45), añ INT, sem INT)
+	BEGIN
+	START TRANSACTION;
+	SET @id_camp = (SELECT id_campeonato FROM campeonato WHERE año = añ AND semestre = sem);
+	SET @id_equ = (SELECT id_equipo FROM equipo WHERE nombre_equipo = nombre_eq);
+	
+	INSERT INTO posicion VALUES(1,0, @id_camp, @id_equ, 0,0);
+	COMMIT;
+	END;
+$$
+DELIMITER ;
+DROP PROCEDURE insertar_gol;
+DELIMITER $$
+CREATE PROCEDURE insertar_gol(minuto INT, tipo_gol VARCHAR(45), ape_jug VARCHAR(45), nom_jug VARCHAR(45),
+							  nombre_loc VARCHAR(45), nombre_vis VARCHAR(45), añ INT, sem INT)
+	BEGIN
+	START TRANSACTION;
+	SET @id_camp = (SELECT id_campeonato FROM campeonato WHERE semestre = sem AND año = añ);
+	SET @id_local = (SELECT id_equipo FROM equipo WHERE nombre_equipo = nombre_loc );
+	SET @id_vis = (SELECT id_equipo FROM equipo WHERE nombre_equipo = nombre_vis );
+	SET @id_partido = (SELECT id_partido FROM partido WHERE id_local = @id_local AND id_visitante = @id_vis 
+					   AND id_campeonato = @id_camp);
+	SET @id_jug = (SELECT id_jugador FROM jugador WHERE nombres_jugador = nom_jug AND apellidos_jugador = ape_jug);
+
+	INSERT INTO gol (tipo_gol, minuto, id_partido, id_jugador )VALUES(tipo_gol, minuto, @id_partido, @id_jug);
+	COMMIT;
+
+	END;
+
+$$
+DELIMITER ;
+
+CALL insertar_arbitro('asdasd', 'asdasd', 'pecora');
+CALL insertar_tecnico('564654465', 7, 'Cruz', 'Andres', 1213213);
+CALL insertar_equipo ('PecoraCity', 'PecoraTeam', 'MALO', 'Cruz', 'Andres');
+CALL insertar_jugador('Cholo', 80, 'Zambrano', 'Pedro', 'PecoraTeam', 0, 10,'1985/03/03');
+CALL insertar_campeonato(2013, 2);
+CALL insertar_partido( curdate(), 'asdsadasd','asdasd', 8, 7, 'PecoraTeam', 'PecoraTeam', 2013, 2 );
+CALL asingar_partido_arbitro('asdasd', 'asdasd', 'PecoraTeam', 'PecoraTeam', 2013, 2);
+CALL insertar_gol (14, 'penal', 'Zambrano', 'Pedro', 'PecoraTeam', 'PecoraTeam', 2013, 2 );
+CALL insertar_equipo_en_campeonato('PecoraTeam',2013,2);
+
+
+
+
+
+
---
+++ Script BD 30-06.sql
@@ -0,0 +1,383 @@
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
+
+
+-- -----------------------------------------------------
+-- Table `tecnico`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `tecnico` ;
+
+CREATE  TABLE IF NOT EXISTS `tecnico` (
+  `id_tecnico` INT NOT NULL AUTO_INCREMENT ,
+  `telefono_tecnico` VARCHAR(45) NULL ,
+  `experiencia_tecnico` INT NULL ,
+  `apellidos_tecnico` VARCHAR(45) NOT NULL ,
+  `nombres_tecnico` VARCHAR(45) NOT NULL ,
+  `salario_tecnico` DOUBLE NULL,
+  PRIMARY KEY (`id_tecnico`) )
+ENGINE = InnoDB;
+
+CREATE INDEX `tecnico_idx` ON `tecnico` (`nombres_tecnico` ASC, `apellidos_tecnico` ASC) ;
+
+CREATE INDEX `experiencia_idx` ON `tecnico` (`id_tecnico` ASC, `experiencia_tecnico` ASC) ;
+
+
+-- -----------------------------------------------------
+-- Table `equipo`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `equipo` ;
+
+CREATE  TABLE IF NOT EXISTS `equipo` (
+  `id_equipo` INT NOT NULL AUTO_INCREMENT ,
+  `id_tecnico` INT NOT NULL ,
+  `ciudad_equipo` VARCHAR(45) NULL ,
+  `nombre_equipo` VARCHAR(45) NOT NULL ,
+  `rendimiento_equipo` VARCHAR(45) NULL ,
+  PRIMARY KEY (`id_equipo`) ,
+  CONSTRAINT `fk_equipo_tecnico1`
+    FOREIGN KEY (`id_tecnico` )
+    REFERENCES `tecnico` (`id_tecnico` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+CREATE INDEX `nombre_ciudad_equipo_UNIQUE` ON `equipo` (`nombre_equipo` ASC, `ciudad_equipo` ASC) ;
+
+CREATE INDEX `fk_equipo_tecnico1_idx` ON `equipo` (`id_tecnico` ASC) ;
+
+CREATE UNIQUE INDEX `tecnico_id_tecnico_UNIQUE` ON `equipo` (`nombre_equipo` ASC, `id_tecnico` ASC) ;
+
+
+-- -----------------------------------------------------
+-- Table `entrenador`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `entrenador` ;
+
+CREATE  TABLE IF NOT EXISTS `entrenador` (
+  `id_entrenador` INT NOT NULL AUTO_INCREMENT ,
+  `id_equipo` INT NOT NULL ,
+  `telefono_entrenador` VARCHAR(45) NULL ,
+  `experiencia_entrenador` INT NULL DEFAULT 0 ,
+  `apellidos_entrenador` VARCHAR(45) NOT NULL ,
+  `nombres_entrenador` VARCHAR(45) NOT NULL ,
+  `salario_entrenador` DOUBLE NOT NULL ,
+  PRIMARY KEY (`id_entrenador`) ,
+  CONSTRAINT `fk_entrenador_equipo1`
+    FOREIGN KEY (`id_equipo` )
+    REFERENCES `equipo` (`id_equipo` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+CREATE INDEX `entrenador_idx` ON `entrenador` (`nombres_entrenador` ASC, `apellidos_entrenador` ASC) ;
+
+CREATE INDEX `fk_entrenador_equipo1_idx` ON `entrenador` (`id_equipo` ASC) ;
+
+CREATE INDEX `entrenador_equipo_idx` ON `entrenador` (`id_entrenador` ASC, `id_equipo` ASC) ;
+
+
+-- -----------------------------------------------------
+-- Table `historico_entrenadores`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `historico_entrenadores` ;
+
+CREATE  TABLE IF NOT EXISTS `historico_entrenadores` (
+  `id_equipo` INT NOT NULL ,
+  `id_entrenador` INT NOT NULL ,
+  `salario_historico_entrenadores` DOUBLE NOT NULL ,
+  `año_historico_entrenadores` INT NOT NULL ,
+  PRIMARY KEY (`id_equipo`, `id_entrenador`) ,
+  CONSTRAINT `fk_historico_entrenadores_equipo1`
+    FOREIGN KEY (`id_equipo` )
+    REFERENCES `equipo` (`id_equipo` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_historico_entrenadores_entrenador1`
+    FOREIGN KEY (`id_entrenador` )
+    REFERENCES `entrenador` (`id_entrenador` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+CREATE INDEX `salario_idx` ON `historico_entrenadores` (`id_entrenador` ASC, `id_equipo` ASC, `salario_historico_entrenadores` ASC) ;
+
+CREATE INDEX `fk_historico_entrenadores_equipo1_idx` ON `historico_entrenadores` (`id_equipo` ASC) ;
+
+CREATE INDEX `fk_historico_entrenadores_entrenador1_idx` ON `historico_entrenadores` (`id_entrenador` ASC) ;
+
+CREATE INDEX `historico_entrenadores_idx` ON `historico_entrenadores` (`id_entrenador` ASC, `id_equipo` ASC) ;
+
+
+-- -----------------------------------------------------
+-- Table `jugador`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `jugador` ;
+
+CREATE  TABLE IF NOT EXISTS `jugador` (
+  `id_jugador` INT NOT NULL AUTO_INCREMENT ,
+  `id_equipo` INT NOT NULL ,
+  `nombres_jugador` VARCHAR(45) NOT NULL ,
+  `apellidos_jugador` VARCHAR(45) NOT NULL ,
+  `nacionalidad_jugador` VARCHAR(45) NOT NULL ,
+  `fechaNacimiento_jugador` DATETIME NOT NULL ,
+  `edad_jugador` INT NULL ,
+  `numero_jugador` INT NOT NULL ,
+  `salario_jugador` INT NULL ,
+  `goles_jugador` INT NULL ,
+  PRIMARY KEY (`id_jugador`) ,
+  CONSTRAINT `fk_jugador_equipo1`
+    FOREIGN KEY (`id_equipo` )
+    REFERENCES `equipo` (`id_equipo` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+CREATE INDEX `jugador_idx` ON `jugador` (`nombres_jugador` ASC, `apellidos_jugador` ASC) ;
+
+CREATE INDEX `nacionalidad_jugador_idx` ON `jugador` (`nombres_jugador` ASC, `apellidos_jugador` ASC, `nacionalidad_jugador` ASC) ;
+
+CREATE INDEX `numero_jugador_idx` ON `jugador` (`nombres_jugador` ASC, `apellidos_jugador` ASC, `numero_jugador` ASC) ;
+
+CREATE INDEX `fk_jugador_equipo1_idx` ON `jugador` (`id_equipo` ASC) ;
+
+
+-- -----------------------------------------------------
+-- Table `historico_jugadores`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `historico_jugadores` ;
+
+CREATE  TABLE IF NOT EXISTS `historico_jugadores` (
+  `id_equipo` INT NOT NULL ,
+  `id_jugador` INT NOT NULL ,
+  `año_historico_jugadores` INT NOT NULL ,
+  `numero_historico_jugadores` INT NOT NULL ,
+  `goles_historico` INT NOT NULL ,
+  PRIMARY KEY (`id_equipo`, `id_jugador`) ,
+  CONSTRAINT `fk_historico_jugadores_equipo1`
+    FOREIGN KEY (`id_equipo` )
+    REFERENCES `equipo` (`id_equipo` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_historico_jugadores_jugador1`
+    FOREIGN KEY (`id_jugador` )
+    REFERENCES `jugador` (`id_jugador` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+CREATE INDEX `numero_historio_idx` ON `historico_jugadores` (`id_jugador` ASC, `id_equipo` ASC, `numero_historico_jugadores` ASC) ;
+
+CREATE INDEX `fk_historico_jugadores_equipo1_idx` ON `historico_jugadores` (`id_equipo` ASC) ;
+
+CREATE INDEX `fk_historico_jugadores_jugador1_idx` ON `historico_jugadores` (`id_jugador` ASC) ;
+
+CREATE INDEX `historico_jugadores_dx` ON `historico_jugadores` (`id_equipo` ASC, `id_jugador` ASC) ;
+
+
+-- -----------------------------------------------------
+-- Table `campeonato`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `campeonato` ;
+
+CREATE  TABLE IF NOT EXISTS `campeonato` (
+  `id_campeonato` INT NOT NULL AUTO_INCREMENT,
+  `año` INT NOT NULL ,
+  `semestre` INT NOT NULL ,
+  PRIMARY KEY (`id_campeonato`) )
+ENGINE = InnoDB;
+
+CREATE INDEX `campeonato_idx` ON `campeonato` (`año` ASC, `semestre` ASC) ;
+
+
+-- -----------------------------------------------------
+-- Table `partido`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `partido` ;
+
+CREATE  TABLE IF NOT EXISTS `partido` (
+  `id_partido` INT NOT NULL AUTO_INCREMENT,
+  `fecha_partido` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ,
+  `ciudad_partido` VARCHAR(45) NOT NULL ,
+  `estadio` VARCHAR(45) NOT NULL ,
+  `goles_local` INT NOT NULL ,
+  `goles_visitante` INT NOT NULL ,
+  `id_local` INT NOT NULL ,
+  `id_visitante` INT NOT NULL ,
+  `id_campeonato` INT NOT NULL ,
+  PRIMARY KEY (`id_partido`) ,
+  CONSTRAINT `id_local`
+    FOREIGN KEY (`id_local` )
+    REFERENCES `equipo` (`id_equipo` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `id_visitante`
+    FOREIGN KEY (`id_visitante` )
+    REFERENCES `equipo` (`id_equipo` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `id_campeonato`
+    FOREIGN KEY (`id_campeonato` )
+    REFERENCES `campeonato` (`id_campeonato` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+CREATE INDEX `id_local_idx` ON `partido` (`id_local` ASC) ;
+
+CREATE INDEX `id_visitante_idx` ON `partido` (`id_visitante` ASC) ;
+
+CREATE INDEX `fecha_estadio_partido_idx` ON `partido` (`fecha_partido` ASC, `estadio` ASC) ;
+
+CREATE INDEX `ciudad_idx` ON `partido` (`id_partido` ASC, `ciudad_partido` ASC) ;
+
+CREATE INDEX `id_campeonato_idx` ON `partido` (`id_campeonato` ASC) ;
+
+CREATE INDEX `contrincantes_idx` ON `partido` (`id_local` ASC, `id_visitante` ASC) ;
+
+
+-- -----------------------------------------------------
+-- Table `gol`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `gol` ;
+
+CREATE  TABLE IF NOT EXISTS `gol` (
+  `id_gol` INT NOT NULL AUTO_INCREMENT,
+  `id_partido` INT NOT NULL ,
+  `id_jugador` INT NOT NULL ,
+  `minuto` INT NOT NULL ,
+  `tipo_gol` VARCHAR(45) NULL ,
+  PRIMARY KEY (`id_gol`) ,
+  CONSTRAINT `id_partido`
+    FOREIGN KEY (`id_partido` )
+    REFERENCES `partido` (`id_partido` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `id_jugador`
+    FOREIGN KEY (`id_jugador` )
+    REFERENCES `jugador` (`id_jugador` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+CREATE UNIQUE INDEX `id_partido_UNIQUE` ON `gol` (`id_partido` ASC) ;
+
+CREATE INDEX `id_jugador_idx` ON `gol` (`id_jugador` ASC) ;
+
+CREATE INDEX `gol_idx` ON `gol` (`id_partido` ASC, `id_jugador` ASC) ;
+
+CREATE INDEX `gol_jugador_idx` ON `gol` (`id_jugador` ASC, `tipo_gol` ASC) ;
+
+
+-- -----------------------------------------------------
+-- Table `arbitro`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `arbitro` ;
+
+CREATE  TABLE IF NOT EXISTS `arbitro` (
+  `id_arbitro` INT NOT NULL AUTO_INCREMENT ,
+  `nombre_arbitro` VARCHAR(45) NOT NULL ,
+  `apellido_arbitro` VARCHAR(45) NOT NULL ,
+  `tipo_arbitro` VARCHAR(45) NULL ,
+  PRIMARY KEY (`id_arbitro`) )
+ENGINE = InnoDB;
+
+CREATE INDEX `arbitro_idx` ON `arbitro` (`nombre_arbitro` ASC, `apellido_arbitro` ASC) ;
+
+CREATE INDEX `tipo_arbitro_idx` ON `arbitro` (`nombre_arbitro` ASC, `apellido_arbitro` ASC, `tipo_arbitro` ASC) ;
+
+
+-- -----------------------------------------------------
+-- Table `posicion`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `posicion` ;
+
+CREATE  TABLE IF NOT EXISTS `posicion` (
+  `id_campeonato` INT NOT NULL ,
+  `id_equipo` INT NOT NULL ,
+  `pos` INT NOT NULL DEFAULT 1 ,
+  `puntaje` INT NOT NULL ,
+  `goles_favor` INT NOT NULL ,
+  `goles_contra` INT NOT NULL ,
+  PRIMARY KEY (`id_campeonato`, `id_equipo`) ,
+  CONSTRAINT `id_campeonato_posicion`
+    FOREIGN KEY (`id_campeonato` )
+    REFERENCES `campeonato` (`id_campeonato` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `id_equipo_posicion`
+    FOREIGN KEY (`id_equipo` )
+    REFERENCES `equipo` (`id_equipo` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+CREATE INDEX `id_campeonato_idx` ON `posicion` (`id_campeonato` ASC) ;
+
+CREATE INDEX `id_equipo_idx` ON `posicion` (`id_equipo` ASC) ;
+
+CREATE UNIQUE INDEX `posicion_idx` ON `posicion` (`id_equipo` ASC, `pos` ASC) ;
+
+CREATE INDEX `equipo_campeonato_idx` ON `posicion` (`id_equipo` ASC, `id_campeonato` ASC) ;
+
+
+-- -----------------------------------------------------
+-- Table `historico_tecnico`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `historico_tecnico` ;
+
+CREATE  TABLE IF NOT EXISTS `historico_tecnico` (
+  `id_equipo` INT NOT NULL ,
+  `id_tecnico` INT NOT NULL ,
+  `telefono_tecnico` VARCHAR(45) NOT NULL ,
+  `experiencia_tecnico` INT NULL ,
+  PRIMARY KEY (`id_equipo`, `id_tecnico`) ,
+  CONSTRAINT `fk_historico_tecnico_equipo1`
+    FOREIGN KEY (`id_equipo` )
+    REFERENCES `equipo` (`id_equipo` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_historico_tecnico_tecnico1`
+    FOREIGN KEY (`id_tecnico` )
+    REFERENCES `tecnico` (`id_tecnico` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+CREATE INDEX `fk_historico_tecnico_equipo1_idx` ON `historico_tecnico` (`id_equipo` ASC) ;
+
+CREATE INDEX `fk_historico_tecnico_tecnico1_idx` ON `historico_tecnico` (`id_tecnico` ASC) ;
+
+CREATE INDEX `historico_entrenadores_idx` ON `historico_tecnico` (`id_tecnico` ASC, `id_equipo` ASC) ;
+
+
+-- -----------------------------------------------------
+-- Table `arbitro_partido`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `arbitro_partido` ;
+
+CREATE  TABLE IF NOT EXISTS `arbitro_partido` (
+  `id_arbitro` INT NOT NULL ,
+  `id_partido` INT NOT NULL ,
+  PRIMARY KEY (`id_arbitro`, `id_partido`) ,
+  CONSTRAINT `id_arbitro_partido`
+    FOREIGN KEY (`id_arbitro` )
+    REFERENCES `arbitro` (`id_arbitro` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `id_partido_partido`
+    FOREIGN KEY (`id_partido` )
+    REFERENCES `partido` (`id_partido` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+CREATE INDEX `id_arbitro_idx` ON `arbitro_partido` (`id_arbitro` ASC) ;
+
+CREATE INDEX `id_partido_idx` ON `arbitro_partido` (`id_partido` ASC) ;
+
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
