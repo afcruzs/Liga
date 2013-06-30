bbee88b4d8152fda6ddd475582cfd762e4e6c01f
Andrés Felipe Cruz <bones.felipe@gmail.com>
2013-06-30 17:16:38 -0500

    Tecnicos actuales

--- Vistas Felipe.sql
+++ Vistas Felipe.sql
@@ -14,7 +14,20 @@ CREATE VIEW ver_jugadores_actuales AS
 		   fechaNacimiento_jugador AS 'Fecha Nacimiento', edad_jugador AS 'Edad', numero_jugador AS 'Número Camiseta', salario_jugador AS 'Salario',
 		   goles_jugador AS 'Goles' FROM jugador NATURAL JOIN equipo;
 
-SELECT * FROM ver_jugadores_actuales;
+#Vista para ver los técnicos actuales
+DROP VIEW IF EXISTS ver_tecnicos_actuales;
+
+CREATE VIEW ver_tecnicos_actuales AS
+	SELECT nombre_equipo AS 'Equipo', nombres_tecnico AS 'Nombres', apellidos_tecnico AS 'Apellidos', experiencia_tecnico AS 'Experiencia', salario_tecnico AS 'Salario'
+		   FROM tecnico NATURAL JOIN equipo;
+
+#Vista para ver datos generales de los equipos
+DROP VIEW IF EXISTS ver_equipos;
+CREATE VIEW ver_equipos AS
+	SELECT nombre_equipo AS 'Nombre', ciudad_equipo AS 'Ciudad', rendimiento_equipo AS 'Rendimiento' FROM equipo;
+
+
+
 
 
 
