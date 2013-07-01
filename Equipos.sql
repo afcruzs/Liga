8779de81ba646afd926903ab509fdc6bbd9a765e
Andrés Felipe Cruz <bones.felipe@gmail.com>
2013-07-01 00:11:43 -0500

    EQuipos
    SUBIDO POR MILDER

---
+++ Equipos.sql
@@ -0,0 +1,39 @@
+-- ---------------------------------
+-- Insertando Campenatos
+-- ---------------------------------
+CALL insertar_campeonato(2013, 2);
+CALL insertar_campeonato(2013, 1);
+CALL insertar_campeonato(2012, 2);
+CALL insertar_campeonato(2012, 1);
+CALL insertar_campeonato(2011, 2);
+CALL insertar_campeonato(2011, 1);
+CALL insertar_campeonato(2010, 2);
+CALL insertar_campeonato(2010, 1);
+CALL insertar_campeonato(2009, 2);
+CALL insertar_campeonato(2009, 1);
+
+
+
+-- -------------------------------
+-- Insertando equipos
+-- -------------------------------
+
+CALL insertar_equipo('Barranquilla','Junior','Alexis','Garcia');
+CALL insertar_equipo('Medellin','Alianza','Héctor','Estrada');
+CALL insertar_equipo('Cali','Cali','Leonel','Alvarez');
+CALL insertar_equipo('Cucuta','Cucuta','Guillermo','Sanguinetti');
+CALL insertar_equipo('Envigado','Envigado','Pedro Enrique','Sarmiento Solis');
+CALL insertar_equipo('Neiva','Huila','Alvaro de Jesus','Gomez');
+CALL insertar_equipo('Itagui','Itagui','Jorge Luis', 'Bernal');
+CALL insertar_equipo('Medellin','Medellin','Hernán Darío','Gómez');
+CALL insertar_equipo('Bogota','Millonarios','Hernán' ,'Torres Oliveros');
+CALL insertar_equipo('Manizales','Onde Caldas','Santiago', 'Escobar');
+CALL insertar_equipo('Pasto','Pasto','Flabio' ,'Torres');
+CALL insertar_equipo('Armenia','Quindio','Eduardo', 'Cruz Rodríguez');
+CALL insertar_equipo('Bogota','Santafe','Wilson' ,'Gutiérrez');
+CALL insertar_equipo('Tunja','Chico','Alberto Miguel', 'Gamero Morillo');
+CALL insertar_equipo('Bogota','Equidad','Néstor', 'Otero');
+CALL insertar_equipo('Medellin','Nacional','Juan Carlos', 'Osorio');
+CALL insertar_equipo('Tunja','Patriotas',' Arturo Ernesto', 'Reyes');
+CALL insertar_equipo('Ibague','Tolima','Carlos', 'Castro');
+
