deee40f02733c0992b4287c08197626c69591cb5
Andrés Felipe Cruz <bones.felipe@gmail.com>
2013-06-30 22:24:38 -0500

    Inserción de entrenadores
    Falta el argumento de entrenadores porque no sé cuales va a meter.

---
+++ Entrenadores.sql
@@ -0,0 +1,20 @@
+#Entrenadores
+
+insertar_entrenador(telefono VARCHAR(45), equipo VARCHAR(45), experiencia INT, 
+										apellidos VARCHAR(45), nombres VARCHAR(45), año INT, salario DOUBLE)
+
+CALL insertar_entrenador('4785210', ,7,'Perez Ramirez', 'Alejandro', 2013, 7450000);
+CALL insertar_entrenador('4781213',,10.'Gutierrez Gonzalez', 'Andrés Camilo', 2013, 13000000);
+CALL insertar_entrenador('7774150',,8, 'Ruiz Sarmiento','Camilo Andrés', 2013, 12000000);
+CALL insertar_entrenador('7456405',,12,'Salinas Angulos', 'Fernando Alonso', 2013, 3000000);
+CALL insertar_entrenador('4894161',,13,'Garzón Galán', 'Jaime Andrés', 2013, 5650000);
+CALL insertar_entrenador('6485110',,20, 'Guzman Perez', 'Yoan Jesus', 2013, 4502065);
+CALL insertar_entrenador('5113501',,17, 'Perez Perez', 'Pilar Andrea', 2013, 15100384);
+CALL insertar_entrenador('4894110',,13,'Ochoa Martinez', 'Oscar Leonardo',2013,1516050);
+CALL insertar_entrenador('5165610',,7,'Fernandez Perez', 'José Camilo',2013,4502065);
+CALL insertar_entrenador('2051018',,7,'Perez Martinez', 'Jaime Felipe',2013,4502065);
+CALL insertar_entrenador('5640890',,2,'Linares Linares', 'Jose Ricardo',2013,4502065);
+CALL insertar_entrenador('4784048',,8,'León Eslava', 'Fernando Camilo',2013,4502065);
+CALL insertar_entrenador('4508940',,12,'Mantilla Gomez', 'José Luis',2013,4502065);
+CALL insertar_entrenador('8798095',,4,'Gomez Errada', 'Camilo',2013,4502065);
+CALL insertar_entrenador('7408940',,15,'Cagua Alvarez', 'Paula Andrea',2013,4502065);
