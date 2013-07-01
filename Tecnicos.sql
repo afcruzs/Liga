01295f72ae59faf8ef021ab94f54a961727f6491
Andrés Felipe Cruz <bones.felipe@gmail.com>
2013-06-30 20:42:57 -0500

    Tecnicos insertados FIXED

---
+++ Tecnicos.sql
@@ -0,0 +1,46 @@
+/*CALL insertar_arbitro('Julián', 'Ruiz', 'Central');
+CALL insertar_tecnico('564654465', 7, 'Cruz', 'Andrés', 1213213);
+CALL insertar_equipo ('PecoraCity', 'PecoraTeam', 'MALO', 'Cruz', 'Andrés');
+
+CALL insertar_jugador('Cholo', 80, 'Zambrano', 'Pedro', 'PecoraTeam', 0, 10,'1985/03/03', 300000);
+CALL insertar_campeonato(2013, 2);
+CALL insertar_partido( curdate(), 'asdsadasd','asdasd', 8, 7, 'PecoraTeam', 'PecoraTeam', 2013, 2 );
+CALL asingar_partido_arbitro('asdasd', 'asdasd', 'PecoraTeam', 'PecoraTeam', 2013, 2);
+CALL insertar_gol (14, 'penal', 'Zambrano', 'Pedro', 'PecoraTeam', 'PecoraTeam', 2013, 2 );
+CALL insertar_equipo_en_campeonato('PecoraTeam',2013,2);
+
+SELECT * FROM posicion;*/
+
+TRUNCATE tecnico;
+
+CALL insertar_tecnico('5789054', 7, 'Estrada', 'Héctor', 2000000); #Alianza
+CALL insertar_tecnico('4787200', 12, 'Alvarez', 'Leonel', 7000000);#Cali
+CALL insertar_tecnico('3084200', 10, 'Sanguinetti', 'Guillermo', 7000000);#Cucuta
+CALL insertar_tecnico('1561615', 6, 'Sarmiento Solís', 'Pedro Enrique', 400000); #Envigado
+CALL insertar_tecnico('7482015', 5,  'Álvaro De Jesús','Gómez', 1520000);#Huila
+CALL insertar_tecnico('6874102', 3, 'Jorge Luis', 'Bernal', 3500000); #Itagui
+CALL insertar_tecnico('6565605', 17, 'Hernán Darío','Gómez', 1000000); #Medellin
+CALL insertar_tecnico('4784185', 20, 'Hernán' ,'Torres Oliveros', 800000);#Millonarios
+CALL insertar_tecnico('4700230', 8, 'Santiago', 'Escobar', 23000000);#Once Caldas
+CALL insertar_tecnico('1561561', 7, 'Flabio' ,'Torres', 10200000); #Pasto
+CALL insertar_tecnico('1215656', 3, 'Eduardo', 'Cruz Rodríguez', 10321000);#Quindío
+CALL insertar_tecnico('7481515', 8, 'Wilson' ,'Gutiérrez', 1400000); #Santafé
+CALL insertar_tecnico('4156115', 5, 'Alberto Miguel', 'Gamero Morillo', 7500000);#Chicó
+CALL insertar_tecnico('4598498', 10, 'Néstor', 'Otero', 4500000);#Equidad
+CALL insertar_tecnico('5161000', 3, 'Alexis', 'García', 44700000);#Junior
+CALL insertar_tecnico('4890984', 7,  'Juan Carlos', 'Osorio', 5000000);#Nacional
+CALL insertar_tecnico('8490909', 8, ' Arturo Ernesto', 'Reyes', 45400000); #Patriotas
+CALL insertar_tecnico('5464065', 6, 'Carlos', 'Castro', 4500000);#Tolima
+
+
+
+
+
+
+
+
+
+
+
+
+
